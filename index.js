5e668ad685e3a7be2fa3a922e66e4d0292b1bfc0
smikulcik <smikulcik@gmail.com>
2018-01-28 23:10:33 -0600

    Remove render method from error handler

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ function logErrors (err, req, res, next) {
 
 function errorHandler (err, req, res, next) {
   res.status(500)
-  res.render('error', { error: err })
+  res.json({ error: err })
 }
 
 app.use(logErrors)
